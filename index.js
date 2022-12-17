0bb15a2824c322cf9c8b24586b333536a0a4cd94
shoque <shoque@users.noreply.github.com>
2022-12-18 05:44:01 +0600

    file renamed

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+const path = require('path')
 const port = process.env.PORT || 5000
 app.use(express.static(path.join(__dirname, 'public')));
 
