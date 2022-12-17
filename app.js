fed027a6a263dbbdc9e6ff63dc2748204f7b3663
shoque <shoque@users.noreply.github.com>
2022-12-18 05:38:44 +0600

    env updated

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
 const express = require('express')
 const app = express()
-const port = 3000 | process.env.NODE_ENV
-app.use(express.static('public'))
-
+const port = process.env.PORT || 5000
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
