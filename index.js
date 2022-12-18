d2bde8a573019effb96876848931128c534a276e
shoque <shoque@users.noreply.github.com>
2022-12-19 03:24:16 +0600

    updated with rtc multi connection

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 const express = require('express')
 const app = express()
 const path = require('path')
+const http = require('http');
+const { Server } = require("socket.io");
+const server = http.createServer(app);
+const rtcServer = require('rtcmulticonnection-server') ;
+const io = new Server(server);
 const port = process.env.PORT || 5000
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.listen(port, () => {
+io.on('connection', (socket) => {
+  rtcServer.addSocket(socket)
+  console.log('a user connected');
+});
+server.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
